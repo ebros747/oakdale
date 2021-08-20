280dd682a056729e69f4f5a26a9c8f0539259298
Randy Kim <RKim@wncinc.com>
2021-08-20 00:03:14 -0700

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,12 +9,12 @@ using Core.Interfaces;
 using Core.Specifications;
 using API.DTO;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
-namespace API.Controllers
+namespace API.Controllers 
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseAPIController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
@@ -45,11 +45,14 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDTO>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
             var product = await _productsRepo.GetEntityWithSpec(spec);
 
+            if(product == null) return NotFound(new ApiResponse(404));
             return _mapper.Map<Product, ProductToReturnDTO>(product);
         }
         
