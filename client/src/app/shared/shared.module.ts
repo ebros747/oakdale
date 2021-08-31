dfa948f5ff2a13b2e311aff3dd00cb0a827e8170
Randy Kim <RKim@wncinc.com>
2021-08-30 22:05:06 -0700

    reinstall cli

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot()
+  ]
+})
+export class SharedModule { }
